1118a3a28c1843db58f88e82a8d79150fcef09b9
UnUnBrainFucker <noam.chanels@gmail.com>
2024-12-05 16:15:52 +0200

    implement parser method for class Compiler

--- Compiler.cpp
+++ Compiler.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "Compiler.h"
 
 using namespace std;
@@ -19,4 +20,55 @@ vector<char> Compiler::tokenize(const string& input) const {
 void Compiler::tokenizer(const string& input) {
     _tokens = tokenize(input);
     parser(_tokens);
+}
+
+void Compiler::parser(const vector<char>& tokens) {
+    ofstream temp(_fileName);
+
+    temp << "#include <iostream>\n";
+    temp << "#include <vector>\n";
+    temp << "#include <string>\n";
+    temp << "\n";
+    temp << "using namespace std;\n";
+    temp << "\n";
+    temp << "int main() {\n";
+    temp << "    vector<int> memory(30000, 0);\n";
+    temp << "    int pointer = 0;\n";
+    temp << "\n";
+
+    for (size_t i = 0; i < tokens.size(); ++i) {
+        char c = tokens[i];
+        switch (c) {
+        case '>':
+            temp << "    ++pointer;\n";
+            break;
+        case '<':
+            temp << "    --pointer;\n";
+            break;
+        case '+':
+            temp << "    ++memory[pointer];\n";
+            break;
+        case '-':
+            temp << "    --memory[pointer];\n";
+            break;
+        case '.':
+            temp << "    cout << static_cast<char>(memory[pointer]);\n";
+            break;
+        case ',':
+            temp << "    memory[pointer] = cin.get();\n";
+            break;
+        case '[':
+            temp << "    while (memory[pointer] != 0) {\n";
+            break;
+        case ']':
+            temp << "    }\n";
+            break;
+        default:
+            break;
+        }
+    }
+    temp << "    return 0;\n";
+    temp << "}\n";
+
+    temp.close();
 }
